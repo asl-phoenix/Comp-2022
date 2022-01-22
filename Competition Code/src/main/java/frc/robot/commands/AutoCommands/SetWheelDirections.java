a5ff63a30644fd8d09677c4e9b2ecaee9abae6a8
GriffinsProgrammers <90404246+GriffinsProgrammers@users.noreply.github.com>
2022-01-22 13:36:59 +0000

    SetWheelDirectionCommand

--- Competition Code/src/main/java/frc/robot/commands/AutoCommands/SetWheelDirections.java
+++ Competition Code/src/main/java/frc/robot/commands/AutoCommands/SetWheelDirections.java
@@ -5,16 +5,29 @@
 package frc.robot.commands.AutoCommands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveRotaters;
 
 public class SetWheelDirections extends CommandBase {
+
+  SwerveRotaters rotators;
+  double fR, fL, bR, bL;
+
   /** Creates a new SetWheelDirections. */
-  public SetWheelDirections() {
+  public SetWheelDirections(SwerveRotaters rotators, double fR, double fL, double bR, double bL) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.rotators = rotators;
+    this.fR = fR;
+    this.fL = fL;
+    this.bR = bR;
+    this.bL = bL;
+    addRequirements(rotators);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    rotators.setWheelDirection(fR, fL, bR, bL);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -27,6 +40,6 @@ public class SetWheelDirections extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return rotators.reachedPosition(fR, fL, bR, bL);
   }
 }
