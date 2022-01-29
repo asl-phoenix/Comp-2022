7751599bbfeb746f882f0329ff3e75a1c5cbb713
GriffinsProgrammers <90404246+GriffinsProgrammers@users.noreply.github.com>
2022-01-29 16:03:39 +0000

    fix

--- Competition Code/src/main/java/frc/robot/commands/AutoCommands/SetWheelDirections.java
+++ Competition Code/src/main/java/frc/robot/commands/AutoCommands/SetWheelDirections.java
@@ -36,7 +36,7 @@ public class SetWheelDirections extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    //otators.stop();
+    rotators.stop();
   }
 
   // Returns true when the command should end.
