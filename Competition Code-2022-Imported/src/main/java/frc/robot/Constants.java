718cf66d0f5bf8f3ff6620a99a0548e53933d0e2
GriffinsProgrammers <90404246+GriffinsProgrammers@users.noreply.github.com>
2022-02-19 13:48:35 +0000

    i hate thids

--- Competition Code-2022-Imported/src/main/java/frc/robot/Constants.java
+++ Competition Code-2022-Imported/src/main/java/frc/robot/Constants.java
@@ -23,8 +23,8 @@ public final class Constants {
 
   // Intake Ports
   public static final int INTAKE_MOTOR_PORT = 10;
-  public static final int INTAKE_PISTON_PORT_1 = 10; // CHECK
-  public static final int INTAKE_PISTON_PORT_2 = 11;
+  public static final int INTAKE_PISTON_PORT_1 = 6; // CHECK
+  public static final int INTAKE_PISTON_PORT_2 = 7;
   public static final int INTAKE_PISTON_PORT_3 = 10;
   public static final int INTAKE_PISTON_PORT_4 = 11;
 
@@ -36,8 +36,8 @@ public final class Constants {
 
   // Catapult Ports
   public static final int SHOOTER_MOTOR_PORT_1 = 11;
-  public static final int SHOOTER_PISTON_PORT_1 = 5;
-  public static final int SHOOTER_PISTON_PORT_2 = 6;
+  public static final int SHOOTER_PISTON_PORT_1 = 4;
+  public static final int SHOOTER_PISTON_PORT_2 = 5;
   public static final int SHOOTER_PISTON_PORT_3 = 3; // CCONFIRM LATER UNTESTED
   public static final int SHOOTER_PISTON_PORT_4 = 2;
 
@@ -47,8 +47,8 @@ public final class Constants {
   // === CONTROLLER === //
 
   // Controller Constants
-  public static final int DRIVER_CONTROLLER = 0; // change back to 0
-  public static final int OPERATOR_CONTROLLER = 1; // change back to 1
+  public static final int DRIVER_CONTROLLER = 1; // change back to 0
+  public static final int OPERATOR_CONTROLLER = 0; // change back to 1
 
   // == BUTTON CONSTANTS == //
 
